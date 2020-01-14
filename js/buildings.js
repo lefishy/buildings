2a0c6716a5dc08abeaf9fd1bd4e95385673c5298
lefishy <lefishy@gmail.com>
2020-01-14 11:45:49 +0000

    chunky

--- js/buildings.js
+++ js/buildings.js
@@ -2,7 +2,7 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( window.innerWidth/2, window.innerHeight/2,false);
 document.body.appendChild( renderer.domElement );
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
