1908f3d356829bd8c53e76ab75269d09357b6e3a
lefishy <lefishy@gmail.com>
2020-01-14 11:41:27 +0000

    cubetest

--- js/buildings.js
+++ js/buildings.js
@@ -4,3 +4,10 @@ var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHe
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
